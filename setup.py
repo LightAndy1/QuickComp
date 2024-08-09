85518233cc16a38b88f098c81480b4ea3631fdfb
Light Andy <andy.197197@yahoo.com>
2024-08-09 17:51:53 +0300

    📦️ Remove GitHub Workflow

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="QuickComp",  # Replace with your package name
-    version="1.0.0",  # This version will be bumped by your GitHub Action
-    packages=find_packages(),  # Automatically find your package and sub-packages
-    include_package_data=True,  # Include other files specified in MANIFEST.in
-)
